baf74bebe5b2d0bdb9e8cec9c326c5f4b8e0f714
ioDat <dat_98@hotmail.com>
2024-02-28 20:13:24 +0100

    D, blockmode

--- TextDecorator.cs
+++ TextDecorator.cs
@@ -4,22 +4,41 @@ class TextDecorator
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /*if (args.Length < 2)
+        {
+            Console.WriteLine("Not enough arguments");
+            return;
+        }
+        */
+        Console.WriteLine("skriv noe");
+        string hei = Console.ReadLine();
+        
+        // decoration type applied to desired
+        // not case sensitive
+
+        // unknown decoration argument or too few arguments => feedback from system
+        // "help" => help function, used as an argument
+
+
+        // Block mode - # Around text #
+        static void BlockMode(string tekst)
+        {
+            string blocks = new string('#', tekst.Length + 4);
+            Console.WriteLine(blocks);
+            Console.WriteLine($"# {tekst} #");
+            Console.WriteLine(blocks);
+        }
+        
+        
+        
+        
+        
+        
+        // Alternate Case Mode - sPoNgE BoB
+
+        //  Pig Latin - Where is my hat? = Erewhay isyay ymay athay?
+
     }
 }
 
-// take in two or more values
-
-// decoration type applied to desired
-// not case sensitive
-
-// unknown decoration argument or too few arguments => feedback from system
-// "help" => help function, used as an argument
-
-
-// Block mode - # Around text #
-
-// Alternate Case Mode - sPoNgE BoB
-
-//  Pog Latin - Where is my hat? = Erewhay isyay ymay athay?
 
