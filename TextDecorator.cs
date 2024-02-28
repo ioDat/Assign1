a4db2a3354299647d763ef70bba81d921ff38d76
ioDat <dat_98@hotmail.com>
2024-02-28 19:21:37 +0100

    Overview

--- TextDecorator.cs
+++ TextDecorator.cs
@@ -7,3 +7,19 @@ class TextDecorator
         Console.WriteLine("Hello, World!");
     }
 }
+
+// take in two or more values
+
+// decoration type applied to desired
+// not case sensitive
+
+// unknown decoration argument or too few arguments => feedback from system
+// "help" => help function, used as an argument
+
+
+// Block mode - # Around text #
+
+// Alternate Case Mode - sPoNgE BoB
+
+//  Pog Latin - Where is my hat? = Erewhay isyay ymay athay?
+
